feaba3200d53d11640961c3109ae70837a571662
dom b <bellybuchelli1234@gmail.com>
2022-02-02 18:17:29 -0500

    layed out funtions made btn classes

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-var numbers = document.querySelectorAll("button")
+var numData = document.querySelectorAll=(".data-number");
 
+function press(num){
+    displayDiv.innerText += num
+}
+function setOP(element){
+    
+}
+function clr(){
+    
+}
+function calculate(){
+
+}
+
+function display(){
+
+}
 
 var displayDiv = document.querySelector("#display");
-displayDiv.innerText = "Some new value";
+displayDiv.innerText = "";
