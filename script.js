91b54970787cc8418f71384f0bb67df923524362
dom b <bellybuchelli1234@gmail.com>
2022-02-02 19:41:51 -0500

    complete

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-var numData = document.querySelectorAll=(".data-number");
 
+var total = "" ;
 function press(num){
     displayDiv.innerText += num
-}
-function setOP(element){
-    
+    total += num
+    console.log(total)
+} 
+function setOP(opt){
+    total += opt
+    displayDiv.innerText= ""
+    console.log(total)
 }
 function clr(){
-    
+    displayDiv.innerText= ""
+    total = ""
 }
 function calculate(){
-
+    displayDiv.innerText= ""
+    total = eval(total).toString();
+    displayDiv.innerText= total
+    console.log(total)
 }
 
-function display(){
 
-}
+var numData = document.querySelectorAll=(".data-number");
+var operator = document.querySelectorAll=(".operator");
 
 var displayDiv = document.querySelector("#display");
 displayDiv.innerText = "";
